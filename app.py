017e0fff83f5ac079c1713b208f7600c7e2579aa
Jonas <jonas-zinne@web.de>
2024-09-18 22:02:09 +0200

    improvements

--- app.py
+++ app.py
@@ -11,28 +11,34 @@ def index():
     if request.method == "POST" and "export_matches" in request.form:
         url = request.form['url']
         message, file_path = main(url)
+        
         session['message'] = message
         session['file_path'] = file_path
-        return redirect(url_for('result'))
+        
+        return redirect(url_for('export_success'))
     
     return render_template("index.html")
 
-# Ergebnis-Seite
-@app.route("/result")
-def result():
+# Erfolgsseite nach dem Export
+@app.route("/export_success")
+def export_success():
     message = session.get('message', '')
     file_path = session.get('file_path', None)
-    file_ready = file_path is not None
-    return render_template("index.html", message=message, file_ready=file_ready, file_path=file_path)
+    
+    if not file_path:
+        return redirect(url_for('index'))
+    
+    return render_template("export_success.html", message=message, file_ready=(file_path is not None))
 
-# Download-Route
-@app.route("/download")
-def download_file():
+# Download der Datei
+@app.route("/download_matches")
+def download_matches():
     file_path = session.get('file_path')
+    
     if file_path and os.path.exists(file_path):
         return send_file(file_path, as_attachment=True)
     else:
         return "Datei nicht gefunden", 404
-    
+
 if __name__ == "__main__":
     app.run(debug=True)
