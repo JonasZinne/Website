3e9c146d0fac63af315fa32d219ae56fe70bcbd2
Jonas <jonas-zinne@web.de>
2024-09-22 00:55:31 +0200

    export vetos

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request, send_file, session
-from export_matches import main
+from export_matches import main as export_matches_main
+from export_vetos import main as export_vetos_main
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -8,20 +9,20 @@ app.secret_key = os.urandom(24)
 # Dashboard (Index)
 @app.route("/", methods=["GET"])
 def index():
-    session['can_download'] = False
+    session['can_download_matches'] = False
     return render_template("index.html")
 
 # Export Matches
 @app.route("/export_matches", methods=["GET", "POST"])
 def export_matches():
-    session['can_download'] = True
+    session['can_download_matches'] = True
     message = None
     file_path = None
     
     if request.method == "POST":
         url = request.form.get('url')
         
-        message, file_path = main(url)      
+        message, file_path = export_matches_main(url)      
         session['message'] = message
         session['file_path'] = file_path
     
@@ -30,7 +31,7 @@ def export_matches():
 # Download der Datei
 @app.route("/download_matches")
 def download_matches():
-    if not session.get('can_download'):
+    if not session.get('can_download_matches'):
         return render_template("error.html", error_code=403, error_message="Download nicht verfügbar"), 403
     
     file_path = session.get('file_path')
@@ -40,10 +41,24 @@ def download_matches():
     else:
         return render_template("error.html", error_code=404, error_message="Datei nicht gefunden"), 404
 
+# Export Vetos
+@app.route("/export_vetos", methods=["GET", "POST"])
+def export_vetos():
+    session['can_download_matches'] = False
+    message = None
+    file_path = None
+    
+    if request.method == "POST":       
+        message, file_path = export_vetos_main()      
+        session['message'] = message
+        session['file_path'] = file_path
+    
+    return render_template("export_vetos.html", message=message, file_ready=(file_path is not None))
+
 # Turnier anlegen
 @app.route("/create_tournament", methods=["GET"])
 def create_tournament():
-    session['can_download'] = False
+    session['can_download_matches'] = False
     return render_template("create_tournament.html")
 
 if __name__ == "__main__":
---
+++ export_vetos.py
@@ -0,0 +1,3 @@
+def main():    
+    message = "Vetos erfolgreich exportiert."
+    return message
