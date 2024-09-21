2b0a7748316b40d9ddf69f20e51da0e03b6253ef
Jonas <jonas-zinne@web.de>
2024-09-22 00:37:50 +0200

    error page

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ app.secret_key = os.urandom(24)
 # Dashboard (Index)
 @app.route("/", methods=["GET"])
 def index():
+    session['can_download'] = False
     return render_template("index.html")
 
 # Export Matches
 @app.route("/export_matches", methods=["GET", "POST"])
 def export_matches():
+    session['can_download'] = True
     message = None
     file_path = None
     
@@ -28,16 +30,20 @@ def export_matches():
 # Download der Datei
 @app.route("/download_matches")
 def download_matches():
+    if not session.get('can_download'):
+        return render_template("error.html", error_code=403, error_message="Download nicht verfügbar"), 403
+    
     file_path = session.get('file_path')
     
     if file_path and os.path.exists(file_path):
         return send_file(file_path, as_attachment=True)
     else:
-        return "Datei nicht gefunden", 404
+        return render_template("error.html", error_code=404, error_message="Datei nicht gefunden"), 404
 
 # Turnier anlegen
 @app.route("/create_tournament", methods=["GET"])
 def create_tournament():
+    session['can_download'] = False
     return render_template("create_tournament.html")
 
 if __name__ == "__main__":
